71166fac6779aa994b3e13423d124425766de1b8
hkalife <51803894+hkalife@users.noreply.github.com>
2021-02-02 18:36:48 -0300

    feat(ship): implement ship movement

---
+++ Assets/Scripts/ShipController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+
+    [SerializeField]
+    private float shipSpeed = 10.0f;
+
+    private float forwardAcceleration = 2.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W)) {
+            shipSpeed = Mathf.Lerp(shipSpeed, 100.0f, forwardAcceleration * Time.deltaTime);
+        } else {
+            shipSpeed = Mathf.Lerp(shipSpeed, 25.0f, forwardAcceleration * Time.deltaTime);
+        }
+        transform.Rotate(Input.GetAxis("Vertical"), 0.0f, -Input.GetAxis("Horizontal"));
+        transform.position += transform.forward * Time.deltaTime * shipSpeed;
+    }
+}
