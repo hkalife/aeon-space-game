7221501be7ada51404ca9961a4bc6217b5cba0ea
hkalife <51803894+hkalife@users.noreply.github.com>
2021-02-03 22:36:15 -0300

    feat(laser): implement laser working

---
+++ Assets/Scripts/LaserController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.isActiveAndEnabled) {
+            transform.position += transform.forward * Time.deltaTime * 500;
+        }
+    }
+}
--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -10,6 +10,12 @@ public class ShipController : MonoBehaviour
 
     private float forwardAcceleration = 2.5f;
 
+    [SerializeField]
+    private GameObject leftLaser;
+
+    [SerializeField]
+    private GameObject rightLaser;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,12 +26,19 @@ public class ShipController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W)) {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.E)) {
             shipSpeed = Mathf.Lerp(shipSpeed, 100.0f, forwardAcceleration * Time.deltaTime);
         } else {
             shipSpeed = Mathf.Lerp(shipSpeed, 25.0f, forwardAcceleration * Time.deltaTime);
         }
-        transform.Rotate(Input.GetAxis("Vertical"), 0.0f, -Input.GetAxis("Horizontal"));
+        transform.Rotate(Input.GetAxis("Vertical") * 5.0f, 0.0f, -Input.GetAxis("Horizontal") * 5.0f);
         transform.position += transform.forward * Time.deltaTime * shipSpeed;
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            GameObject newLeftLaser = Instantiate(leftLaser, leftLaser.transform.position, leftLaser.transform.rotation);
+            GameObject newRightLaser = Instantiate(rightLaser, rightLaser.transform.position, rightLaser.transform.rotation);
+            newLeftLaser.SetActive(true);
+            newRightLaser.SetActive(true);
+        }
     }
 }
