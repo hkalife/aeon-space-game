9a5dde95ffd6a94d8976255949f2f2015d34c734
hkalife <51803894+hkalife@users.noreply.github.com>
2021-02-06 19:18:21 -0300

    feat(ship): add mouse functions

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -26,7 +26,7 @@ public class ShipController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.E)) {
+        if (Input.GetKey(KeyCode.W)) {
             shipSpeed = Mathf.Lerp(shipSpeed, 100.0f, forwardAcceleration * Time.deltaTime);
         } else {
             shipSpeed = Mathf.Lerp(shipSpeed, 25.0f, forwardAcceleration * Time.deltaTime);
@@ -34,7 +34,7 @@ public class ShipController : MonoBehaviour
         transform.Rotate(Input.GetAxis("Vertical") * 5.0f, 0.0f, -Input.GetAxis("Horizontal") * 5.0f);
         transform.position += transform.forward * Time.deltaTime * shipSpeed;
 
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) {
             GameObject newLeftLaser = Instantiate(leftLaser, leftLaser.transform.position, leftLaser.transform.rotation);
             GameObject newRightLaser = Instantiate(rightLaser, rightLaser.transform.position, rightLaser.transform.rotation);
             newLeftLaser.SetActive(true);
